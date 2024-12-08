e41cd1edf79c661204376665a76784120275d312
Andrew Updegrove <aupde3@gmail.com>
2024-12-08 19:58:31 +0000

    paramaterized solution execution

--- day_1/solution.ts
+++ day_1/solution.ts
@@ -1,39 +1,42 @@
-import { parseInput } from "@utilities/parser"
+import { SolutionBase } from "@utilities/solutionFormat"
 
-function part_1(filepath: string) {
-  const locID1: number[] = []
-  const locID2: number[] = []
-  parseInput(filepath, (line) => {
-    const [loc1, loc2] = line.split('  ')
-    locID1.push(Number(loc1))
-    locID2.push(Number(loc2))
-  })
-  locID1.sort()
-  locID2.sort()
 
-  const cumDist = locID1.reduce((cum, curr, index) => {
-    cum += Math.abs(curr - locID2[index])
-    return cum
-  }, 0)
-  return cumDist
+export default class Day1 extends SolutionBase {
+  constructor(filepath: string) {
+    super(`${__dirname}/${filepath}`)
+
+  }
+  part_1() {
+    const locID1: number[] = []
+    const locID2: number[] = []
+    this.parseInput((line) => {
+      const [loc1, loc2] = line.split('  ')
+      locID1.push(Number(loc1))
+      locID2.push(Number(loc2))
+    })
+    locID1.sort()
+    locID2.sort()
+
+    const cumDist = locID1.reduce((cum, curr, index) => {
+      cum += Math.abs(curr - locID2[index])
+      return cum
+    }, 0)
+    return cumDist
+  }
+
+  part_2() {
+    const locMapRight = new Map<number, number>()
+    const locMapLeft = new Map<number, number>()
+    this.parseInput((line) => {
+      const [loc1, loc2] = line.split('  ').map(Number)
+      locMapLeft.set(loc1, (locMapLeft.get(loc1) ?? 0) + 1)
+      locMapRight.set(loc2, (locMapRight.get(loc2) ?? 0) + 1)
+
+    })
+    let similarity = 0
+    locMapLeft.forEach( (occur, id) => {
+      similarity += (id * occur * (locMapRight.get(id) ?? 0))
+    })
+    return similarity
+  }
 }
-
-function part_2(filepath: string) {
-  const locMapRight = new Map<number, number>()
-  const locMapLeft = new Map<number, number>()
-  parseInput(filepath, (line) => {
-    const [loc1, loc2] = line.split('  ').map(Number)
-    locMapLeft.set(loc1, (locMapLeft.get(loc1) ?? 0) + 1)
-    locMapRight.set(loc2, (locMapRight.get(loc2) ?? 0) + 1)
-
-  })
-  let similarity = 0
-  locMapLeft.forEach( (occur, id) => {
-    similarity += (id * occur * (locMapRight.get(id) ?? 0))
-  })
-  return similarity
-}
-
-const filepath = 'day_1/input.txt'
-console.log(`Solution 1: ${part_1(filepath)}`)
-console.log(`Solution 2: ${part_2(filepath)}`)
