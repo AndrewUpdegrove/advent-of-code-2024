ae22ea66ef189ef07056ab3bc77cf6946ea9e309
Andrew Updegrove <aupde3@gmail.com>
2024-12-18 00:07:15 +0000

    Return shape of the input in parseInput abstract

--- utilities/solutionFormat.ts
+++ utilities/solutionFormat.ts
@@ -13,11 +13,26 @@ export abstract class SolutionBase implements ISolution {
   abstract part_1(): unknown
   abstract part_2(): unknown
 
-  parseInput (processLine: (line: string, index: number, array: string[]) => void): void {
+  parseInput (processLine: (line: string, index: number, array: string[]) => void): Array<number> {
     const data = readFileSync(this.inputFile, { encoding: 'utf-8' })
     const lines = data.split('\n')
+    const rows = lines.length
+    let cols: number
     lines.forEach((line, index, array) => {
+      if (cols === undefined) cols = line.length
+      else {
+        if (line.length !== cols){
+          console.warn('Line length of input varies. Returning length of longest line in shape')
+          if (line.length > cols) cols = line.length
+        }
+      }
       processLine(line, index, array)
     })
+    // @ts-expect-error cols used before being assigned. I know. that's why I'm checking if its undefined
+    if (cols === undefined) {
+      console.warn('No lines were processed')
+      cols = 0
+    }
+    return [rows, cols]
   }
 }
