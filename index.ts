70a62cf2127ba1c2b0d06f55625411f42516aca6
Andrew Updegrove <aupde3@gmail.com>
2025-01-12 16:42:55 +0000

    Add ability to pass in more args to solution

--- index.ts
+++ index.ts
@@ -9,12 +9,13 @@ parser.add_argument('-d', '--day', {type: 'int', required: true, help: 'Day of t
 parser.add_argument('-f', '--file', { type: 'str', required: true, help: "what input file to run the parts on automatically adds .txt. File must reside in the specific day's folder"});
 parser.add_argument('-1', '--one', { action: 'store_true', help: 'Run part 1 of the solution; if neither 1 or 2 flag are supplied both are run'})
 parser.add_argument('-2', '--two', { action: 'store_true', help: 'run part 2 of the solution; if neither 1 or 2 flag are supplied both are run'});
+parser.add_argument('-i', '--inputs', { type: 'str', nargs: '*', help: 'Any additional variable arguments that aren\'t specified in the input file'})
 
 const args = parser.parse_args()
 
 import(`day_${args.day}/solution`).then((sol) => {
   const Solution = sol.default
-  const runner: ISolution = new Solution(`${args.file}.txt`)
+  const runner: ISolution = new Solution(`${args.file}.txt`, args.inputs) 
   if (args.one || args.one === args.two) {
     console.log(`Part 1 answer: ${runner.part_1()}`)
   }
